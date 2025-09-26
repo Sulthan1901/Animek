d0d0074a441987aa747859ed624a6063e2480be3
Sulthan1901 <sulthanzaki1901@gmail.com>
2025-09-26 15:48:20 +0700

    Menambah screens pages

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pertemuan/main.dart';
+import 'package:anime_verse/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
